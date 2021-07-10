5efa34c054732a69725c183e21384980d85eb77a
Keert <nkeertana11@gmail.com>
2021-07-10 16:56:59 +0530

    rewarding fitness

---
+++ app/src/main/java/com/project/rewardingfitnessapp/Coupons.java
@@ -0,0 +1,24 @@
+package com.project.rewardingfitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Coupons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coupons);
+    }
+
+    public void Food(View view) {
+    }
+
+    public void Cashback(View view) {
+    }
+
+    public void Giftvouncher(View view) {
+    }
+}
---
+++ app/src/main/java/com/project/rewardingfitnessapp/FitnessGames.java
@@ -0,0 +1,30 @@
+package com.project.rewardingfitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class FitnessGames extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fitness_games);
+    }
+
+    public void Zumba(View view) {
+    }
+
+    public void Pushup(View view) {
+    }
+
+    public void Boxing(View view) {
+    }
+
+    public void Balance(View view) {
+    }
+
+    public void Pushups(View view) {
+    }
+}
